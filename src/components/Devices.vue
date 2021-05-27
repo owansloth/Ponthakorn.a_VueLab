38b8f2bff229e43254ee03ec3bcf7dda66c2d5ff
owansloth <84859706+owansloth@users.noreply.github.com>
2021-05-27 16:58:25 +0700

    Initial commit

---
+++ src/components/Devices.vue
@@ -0,0 +1,65 @@
+<template>
+  <h1 class="font-black mb-3">Devices</h1>
+  <div class="grid grid-cols-3 gap-4 border border-black ml-0 mr-5 p-3">
+    <ul v-for="device in devices" :key="device.id">
+      <li>
+        <router-link
+          :to="{
+            name: 'device',
+            params: { id: device.id },
+          }"
+        >
+          <img :src="device.Photo" />
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  props: {
+    manufacturerId: Number,
+  },
+  data: () => ({
+    devices: [
+      { id: 1, DeviceName: "ASUS ROG" },
+      { id: 2, DeviceName: "HP PAVILION" },
+    ],
+  }),
+  created() {
+    console.log("manufacturerId :", this.manufacturerId);
+    if (this.manufacturerId !== 0) {
+      axios
+        .get(
+          `https://service2.ahead-coop.work/devices?ManufacturerID=${this.manufacturerId}`
+        )
+        .then((res) => {
+          this.devices = res.data;
+        });
+    } else {
+      axios.get(`https://service2.ahead-coop.work/devices`).then((res) => {
+        this.devices = res.data;
+      });
+    }
+  },
+  watch: {
+    manufacturerId: function (newId, oldId) {
+      console.log(`new: ${newId}, old: ${oldId}`);
+      this.getDeviceByManufacturer(newId);
+    },
+  },
+  methods: {
+    getDeviceByManufacturer(id) {
+      axios
+        .get(
+          `https://service2.ahead-coop.work/devices?ManufacturerID=${this.manufacturerId}`
+        )
+        .then((res) => {
+          this.devices = res.data;
+        });
+    },
+  },
+};
+</script>
